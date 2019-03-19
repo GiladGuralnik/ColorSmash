acaa5d2a8e8454f0b0f9f82239ea18b09cabc153
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-03-19 10:38:45 +0200

    added Admin Option page

---
+++ app/src/main/java/com/example/colorsmash/AdminOptions.java
@@ -0,0 +1,30 @@
+package com.example.colorsmash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AdminOptions extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonShowUsers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_options);
+
+        buttonShowUsers = (Button) findViewById(R.id.ButtonShowUsers);
+        buttonShowUsers.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonShowUsers) {
+            Intent act = new Intent(AdminOptions.this, RegisterActivity.class);
+            startActivity(act);
+        }
+    }
+}
