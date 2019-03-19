1c56902d5dad6c27758f71bf9f1086ddcea0f19e
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-03-19 12:51:52 +0200

    create dynamic table

--- app/src/main/java/com/example/colorsmash/ShowUsers.java
+++ app/src/main/java/com/example/colorsmash/ShowUsers.java
@@ -3,6 +3,7 @@ package com.example.colorsmash;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -33,10 +34,13 @@ public class ShowUsers extends AppCompatActivity {
         mDataBase= FirebaseDatabase.getInstance();
         mRef = mDataBase.getReference("Users");
 
+
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
                 showData(dataSnapshot);
+                func();
             }
 
 
@@ -47,16 +51,76 @@ public class ShowUsers extends AppCompatActivity {
         });
 
 
-        showUsers();
+
+        //showUsers();
     }
 
+    //Initiates the table
+    public void init(){
+
+        //This part defines the layout to be used for creating new rows
+        TableLayout ll = (TableLayout) findViewById(R.id.UsersTable);
+        TableRow row= new TableRow(this);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(lp);
+        TextView tName = new TextView(this);
+        TextView tUsername = new TextView(this);
+        TextView tAge = new TextView(this);
+        TextView tGender = new TextView(this);
+
+        //This generates the caption row
+        tName.setText("Name");
+        tName.setPadding(3, 3, 3, 3);
+        tUsername.setText("Username");
+        tUsername.setPadding(3, 3, 3, 3);
+        tAge.setText("Age");
+        tAge.setPadding(3, 3, 3, 3);
+        tGender.setText("Gender");
+        tGender.setPadding(3, 3, 3, 3);
+        row.addView(tName);
+        row.addView(tUsername);
+        row.addView(tAge);
+        row.addView(tGender);
+        ll.addView(row,0);
+
+        //This loop adds the database content to the table (using hardcoded values here for demonstration)
+        for (int i = 0; i < users.size(); i++) {
+
+            row= new TableRow(this);
+            lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            tName = new TextView(this);
+            tUsername = new TextView(this);
+            tAge = new TextView(this);
+            tGender = new TextView(this);
+            tName.setText(users.get(i).getName());
+            tName.setPadding(3, 3, 3, 3);
+            tUsername.setText(users.get(i).getUsername());
+            tUsername.setPadding(3, 3, 3, 3);
+            tAge.setText(Integer.toString(users.get(i).getAge()));
+            tAge.setPadding(3, 3, 3, 3);
+            tGender.setText(users.get(i).getGender());
+            tGender.setPadding(3, 3, 3, 3);
+
+            row.addView(tName);
+            row.addView(tUsername);
+            row.addView(tAge);
+            row.addView(tGender);
+            ll.addView(row,i);
+        }
+    }
+
+    public void func(){
+        init();
+        Log.d("Asdasdasd",users.get(0).getUsername());
+    }
     public void showUsers() {
 
-        TableLayout prices = (TableLayout)findViewById(R.id.UsersTable);
-        prices.setStretchAllColumns(true);
-        prices.bringToFront();
-        for(int i = 0; i < 2; i++){
-            TableRow tr =  new TableRow(this);
+        TableLayout usersInfo = (TableLayout) findViewById(R.id.UsersTable);
+        usersInfo.setStretchAllColumns(true);
+        usersInfo.bringToFront();
+        for (int i = 0; i < 1; i++) {
+            TableRow tr = new TableRow(this);
             TextView c1 = new TextView(this);
             c1.setText(users.get(i).getName());
             TextView c2 = new TextView(this);
@@ -70,7 +134,7 @@ public class ShowUsers extends AppCompatActivity {
             tr.addView(c2);
             tr.addView(c3);
             tr.addView(c4);
-            prices.addView(tr);
+            usersInfo.addView(tr);
         }
 
     }
