fdd607edc0e9da4c26a7eaa01a86ceee7a47d4bc
Reuven Regev Farag <regevfarag@gmail.com>
2019-03-20 13:06:32 +0200

    added new layer to backup branch

--- app/src/main/java/com/example/colorsmash/RegisterActivity.java
+++ app/src/main/java/com/example/colorsmash/RegisterActivity.java
@@ -165,6 +165,42 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         index = maxKey+1;
     }
 
+    private void doSomething()
+    {
+        int arr[] ={3,60,35,2,45,320,5};
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+
+        n = arr.length;
+        temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+
+
+
+    }
+
+
 
 
 
