209b2b7bbe003237ff4eb62a334637767314ccb3
Yoel Vaizman <yoelv92@gmail.com>
2019-03-21 13:25:29 +0200

    startin unit test in login activity

--- app/src/main/java/com/example/colorsmash/LoginActivity.java
+++ app/src/main/java/com/example/colorsmash/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -74,6 +75,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 progressDialog.dismiss();
                 //GO TO MAIN ACTIVITY
                 if(task.isSuccessful()){
+                    Log.d("yoelTest","yyyooyoyoy");//!!!!!!!!!!!!!!
                     finish();
                     startActivity(new Intent(getApplicationContext() , StartGameActivity.class));
 
---
+++ app/src/test/java/com/example/colorsmash/LoginActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.colorsmash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Test
+    public void onCreate() {
+
+
+    }
+
+    @Test
+    public void onClick() {
+    }
+}
