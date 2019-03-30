af6be0b211933ebbd31c4bf5ea5f586e5ce0ad7f
Reuven Regev Farag <regevfarag@gmail.com>
2019-03-30 11:17:27 +0300

    some changes

--- app/src/main/java/com/example/colorsmash/LoginActivity.java
+++ app/src/main/java/com/example/colorsmash/LoginActivity.java
@@ -62,16 +62,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    public boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    public boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
     public final boolean isValidEmail(CharSequence target) {
         if(TextUtils.isEmpty(target)){
             Toast.makeText(this,"Please enter email", Toast.LENGTH_LONG).show();
