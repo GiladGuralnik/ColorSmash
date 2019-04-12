026421541f543e262d3380d883917f066bb05be7
Reuven Regev Farag <regevfarag@gmail.com>
2019-04-12 14:25:59 +0300

    T2-118 define frame width and height

--- app/src/main/java/com/example/colorsmash/StartGameActivity.java
+++ app/src/main/java/com/example/colorsmash/StartGameActivity.java
@@ -2,11 +2,15 @@ package com.example.colorsmash;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -14,40 +18,39 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class StartGameActivity extends AppCompatActivity {
 
+    //GameFrame
+    private FrameLayout gameFrame;
+    private int frameHeight,frameWidth,initialFrameWidth;
+    private LinearLayout startLayout;
 
-    private Button startGameButton;
-    private Button logoutButton;
-    private TextView scoreViewText;
+    //Images
+    private ImageView box,black,orange,pink;
+    private Drawable imageBoxRight,imageBoxLeft;
 
+    //Size
+    private int boxSize;
 
-    //private FirebaseAuth firebaseAuth;
-    //private ProgressDialog progressDialog;
+    //position
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
 
-        //firebaseAuth=FirebaseAuth.getInstance();
-
-        startGameButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                    startGame();
-            }
-        });
-
-        logoutButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                finish();
-                startActivity(new Intent(getApplicationContext() , MainActivity.class));
-            }
-        });
 
     }
-    private void startGame(){
-        scoreViewText.setText("999");
+
+    public void startGame(View view)
+    {
+
     }
 
+
+    public void exitGame(View view)
+    {
+
+    }
 
 
 }
