019bf289f6b1a4595233439c640751a19158642c
Yoel Vaizman <yoelv92@gmail.com>
2019-04-29 22:34:00 +0300

    T2-154 finish cant see button

--- app/src/main/java/com/example/colorsmash/DiagnosisActivity.java
+++ app/src/main/java/com/example/colorsmash/DiagnosisActivity.java
@@ -4,20 +4,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DiagnosisActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private Button submit ;//buttonSubmitDiagnosis
-    private Button question1 ;//buttonQuestion6Diagnosis
-    private Button question2 ;
-    private Button question3 ;
-    private Button question4 ;
-    private Button question5 ;
-    private Button question6 ;
+    private Button buttonQuestion1 ,buttonQuestion2,buttonQuestion3,buttonQuestion4,buttonQuestion5,buttonQuestion6;//button Question Diagnosis
+    boolean[] questionButtonFlags = new boolean[6];
+    private EditText textQuestion1,textQuestion2,textQuestion3,textQuestion4,textQuestion5,textQuestion6 ;//editTextQuestion1Diagnosis
 
 
 
@@ -27,21 +29,23 @@ public class DiagnosisActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnosis);
 
+        Arrays.fill(questionButtonFlags, Boolean.FALSE);
+
         submit=(Button)findViewById(R.id.buttonSubmitDiagnosis);
-        question1=(Button)findViewById(R.id.buttonQuestion1Diagnosis);
-        question2=(Button)findViewById(R.id.buttonQuestion2Diagnosis);
-        question3=(Button)findViewById(R.id.buttonQuestion3Diagnosis);
-        question4=(Button)findViewById(R.id.buttonQuestion4Diagnosis);
-        question5=(Button)findViewById(R.id.buttonQuestion5Diagnosis);
-        question6=(Button)findViewById(R.id.buttonQuestion6Diagnosis);
+        buttonQuestion1=(Button)findViewById(R.id.buttonQuestion1Diagnosis);
+        buttonQuestion2=(Button)findViewById(R.id.buttonQuestion2Diagnosis);
+        buttonQuestion3=(Button)findViewById(R.id.buttonQuestion3Diagnosis);
+        buttonQuestion4=(Button)findViewById(R.id.buttonQuestion4Diagnosis);
+        buttonQuestion5=(Button)findViewById(R.id.buttonQuestion5Diagnosis);
+        buttonQuestion6=(Button)findViewById(R.id.buttonQuestion6Diagnosis);
 
         submit.setOnClickListener(this);
-        question1.setOnClickListener(this);
-        question2.setOnClickListener(this);
-        question3.setOnClickListener(this);
-        question4.setOnClickListener(this);
-        question5.setOnClickListener(this);
-        question6.setOnClickListener(this);
+        buttonQuestion1.setOnClickListener(this);
+        buttonQuestion2.setOnClickListener(this);
+        buttonQuestion3.setOnClickListener(this);
+        buttonQuestion4.setOnClickListener(this);
+        buttonQuestion5.setOnClickListener(this);
+        buttonQuestion6.setOnClickListener(this);
 
 
 
@@ -53,43 +57,45 @@ public class DiagnosisActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view){
         if(view == submit){
-            String url = "https://enchroma.com/pages/test";
-
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(url));
-            startActivity(i);
-        }
-        else if(view == question1){
-
-            if(question1.getText().toString()=="Enter the number you see?"){
-                question1.setBackgroundColor(getResources().getColor(R.color.color_red));
-                question1.setText("Remove");
-
-            }
-            else{
-                question1.setBackgroundColor(getResources().getColor(R.color.color_blue));
-                question1.setText("Enter the number you see?");
-
-            }
-            //need to check !!!!!
-        }
-
-        else if(view == question2){
 
         }
-        else if(view == question3){
-
-        }
-        else if(view == question4){
-
-        }
-        else if(view == question5){
-
-        }
-        else if(view == question6){
-
+        else if(view == buttonQuestion1){
+            questionButtonFlags[1]=changeButton(buttonQuestion1);
+            Log.d("TTT", String.valueOf(questionButtonFlags[1]));
         }
 
+        else if(view == buttonQuestion2){
+            questionButtonFlags[2]=changeButton(buttonQuestion2);
+        }
+        else if(view == buttonQuestion3){
+            questionButtonFlags[3]=changeButton(buttonQuestion3);
+        }
+        else if(view == buttonQuestion4){
+            questionButtonFlags[4]=changeButton(buttonQuestion4);
+        }
+        else if(view == buttonQuestion5){
+            questionButtonFlags[5]=changeButton(buttonQuestion5);
+        }
+        else if(view == buttonQuestion6){
+            questionButtonFlags[6]=changeButton(buttonQuestion6);
+        }
+
+    }
+    Boolean changeButton(Button button)
+    {
+        //This function it to change
+
+        if(button.getText().toString()=="Can not see anything"){
+            button.setBackgroundColor(getResources().getColor(R.color.color_red));
+            button.setText("I see");
+            return true;
+
+        }
+        else{
+            button.setBackgroundColor(getResources().getColor(R.color.color_blue));
+            button.setText("Can not see anything");
+            return false;
+        }
     }
 
 }
