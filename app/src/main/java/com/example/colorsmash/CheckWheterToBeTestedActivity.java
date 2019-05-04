025df235384533bcc0b1cb83d17093ff2b9f8348
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-05-04 21:58:44 +0300

    T2-139 ask the user for color blind test

---
+++ app/src/main/java/com/example/colorsmash/CheckWheterToBeTestedActivity.java
@@ -0,0 +1,13 @@
+package com.example.colorsmash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckWheterToBeTestedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_wheter_to_be_tested);
+    }
+}
