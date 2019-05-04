f8a30296acec4a79e64500d53787c22b1af12bd4
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-05-04 22:10:59 +0300

    T2-139 create ask for test page activity

--- app/src/main/java/com/example/colorsmash/CheckWheterToBeTestedActivity.java
+++ app/src/main/java/com/example/colorsmash/CheckWheterToBeTestedActivity.java
@@ -1,13 +1,42 @@
 package com.example.colorsmash;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class CheckWheterToBeTestedActivity extends AppCompatActivity {
+public class CheckWheterToBeTestedActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button buttonYesTest;
+    private Button buttonNoTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_wheter_to_be_tested);
+
+        buttonYesTest = (Button)findViewById(R.id.ButtonYesTest);
+        buttonNoTest = (Button)findViewById(R.id.ButtonNoTest);
+
+        buttonNoTest.setOnClickListener(this);
+        buttonYesTest.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonYesTest) {
+            Intent act = new Intent(this, DiagnosisActivity.class);
+            startActivity(act);
+
+        }
+
+        else if (view == buttonNoTest){
+            Intent act = new Intent(this, StartGameActivity.class);
+            startActivity(act);
+        }
     }
 }
